d5152e44be0ed1a95d0f8656aecf7de95a255ffe
YoumiMa <youmima1015@gmail.com>
2020-08-21 13:38:20 +0900

    cleared pycache

---
+++ table_filling.py
@@ -0,0 +1,41 @@
+import argparse
+
+from args import train_argparser, eval_argparser
+from config_reader import process_configs
+from table_filling import input_reader
+from table_filling.tabF_trainer import TableFTrainer
+
+
+def __train(run_args):
+    trainer = TableFTrainer(run_args)
+    trainer.train(train_path=run_args.train_path, valid_path=run_args.valid_path,
+                  types_path=run_args.types_path, input_reader_cls=input_reader.JsonInputReader)
+
+
+def _train():
+    arg_parser = train_argparser()
+    process_configs(target=__train, arg_parser=arg_parser)
+
+
+def __eval(run_args):
+    trainer = TableFTrainer(run_args)
+    trainer.eval(dataset_path=run_args.dataset_path, types_path=run_args.types_path,
+                 input_reader_cls=input_reader.JsonInputReader)
+
+
+def _eval():
+    arg_parser = eval_argparser()
+    process_configs(target=__eval, arg_parser=arg_parser)
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(add_help=False)
+    arg_parser.add_argument('mode', type=str, help="Mode: 'train' or 'eval'")
+    args, _ = arg_parser.parse_known_args()
+
+    if args.mode == 'train':
+        _train()
+    elif args.mode == 'eval':
+        _eval()
+    else:
+        raise Exception("Mode not in ['train', 'eval'], e.g. 'python table_filling.py train ...'")
