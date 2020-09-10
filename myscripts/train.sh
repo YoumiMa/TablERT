c8a5105dbdc8461088b04272c8a64ddfd14b399e
acb11709gz <acb11709gz@g0050.abci.local>
2020-09-10 15:10:38 +0900

    removed pycache

--- myscripts/train.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=5 nohup python -W ignore:semaphore_tracker:UserWarning ./spert.py train --config configs/train_bioul_conll03_base.conf > conll03_bert_doc_5runs&
-ps -f | egrep "$$|PID"
